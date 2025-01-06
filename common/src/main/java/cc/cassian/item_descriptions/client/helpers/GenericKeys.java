6fc22a4e3ae7767c6126f34a48d763805e2e6c3b
sisby-folk <nora.herrah.heartstrings@gmail.com>
2025-01-06 22:57:27 +1100

    prioritize longer tags

--- common/src/main/java/cc/cassian/item_descriptions/client/helpers/GenericKeys.java
+++ common/src/main/java/cc/cassian/item_descriptions/client/helpers/GenericKeys.java
@@ -3,6 +3,7 @@ package cc.cassian.item_descriptions.client.helpers;
 import cc.cassian.item_descriptions.client.config.ModConfig;
 import net.minecraft.block.*;
 import net.minecraft.client.resource.language.I18n;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -106,7 +107,7 @@ public class GenericKeys {
             final String[] returnedKey = new String[1];
             itemStack.streamTags().forEach(itemTagKey -> {
                 String loreKey = "tag."+itemTagKey.id().toTranslationKey()+".description";
-                if (I18n.hasTranslation(loreKey)) {
+                if (I18n.hasTranslation(loreKey) && (returnedKey[0] == null || returnedKey[0].length() < loreKey.length())) {
                     returnedKey[0] = loreKey;
                 }
             });
@@ -115,7 +116,7 @@ public class GenericKeys {
                 if ((item instanceof BlockItem blockItem)) {
                     blockItem.getBlock().getDefaultState().streamTags().forEach(itemTagKey -> {
                         String loreKey = "tag."+itemTagKey.id().toTranslationKey()+".description";
-                        if (I18n.hasTranslation(loreKey)) {
+                        if (I18n.hasTranslation(loreKey) && (returnedKey[0] == null || returnedKey[0].length() < loreKey.length())) {
                             returnedKey[0] = loreKey;
                         }
                     });
@@ -128,12 +129,23 @@ public class GenericKeys {
             final String[] returnedKey = new String[1];
             state.streamTags().forEach(itemTagKey -> {
                 String loreKey = "tag."+itemTagKey.id().toTranslationKey()+".description";
-                if (I18n.hasTranslation(loreKey)) {
+                if (I18n.hasTranslation(loreKey) && (returnedKey[0] == null || returnedKey[0].length() < loreKey.length())) {
                     returnedKey[0] = loreKey;
                 }
             });
             return returnedKey[0];
         }
+        else if ((object instanceof Entity entity)) {
+            final String[] returnedKey = new String[1];
+            entity.getType().getRegistryEntry().streamTags().forEach(itemTagKey -> {
+                String loreKey = "tag."+itemTagKey.id().toTranslationKey()+".description";
+                if (I18n.hasTranslation(loreKey) && (returnedKey[0] == null || returnedKey[0].length() < loreKey.length())) {
+                    returnedKey[0] = loreKey;
+                }
+            });
+            return returnedKey[0];
+        }
+
         //If no tag key matches, return empty so a string match can be found.
         else return "";
     }
