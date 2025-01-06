05b4241f066a0517b17c488c20db53b6f6b81461
sisby-folk <nora.herrah.heartstrings@gmail.com>
2025-01-07 00:57:32 +1100

    config config paths

--- forge/src/main/java/cc/cassian/item_descriptions/client/config/forge/ModConfigImpl.java
+++ forge/src/main/java/cc/cassian/item_descriptions/client/config/forge/ModConfigImpl.java
@@ -5,10 +5,8 @@ import net.minecraftforge.fml.loading.FMLLoader;
 
 import java.nio.file.Path;
 
-import static cc.cassian.item_descriptions.client.ModClient.MOD_ID_NEO;
-
 public class ModConfigImpl {
     public static Path configPath() {
-        return Path.of(FMLLoader.getGamePath() + "/config").resolve(MOD_ID_NEO + ".json");
+        return Path.of(FMLLoader.getGamePath() + "/config");
     }
 }
